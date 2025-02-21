2507ab8da204d570d81d4a3f3054256391cbf73a
Khanh Duy <duylekhanh.jeff1002@gmail.com>
2025-02-21 16:08:12 +0700

    Initial Commit

---
+++ ListingApplication/ListingApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListingApplicationApp.swift
+//  ListingApplication
+//
+//  Created by Admin on 21/2/25.
+//
+
+import SwiftUI
+
+@main
+struct ListingApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ListingApplicationTests/ListingApplicationTests.swift
@@ -0,0 +1,17 @@
+//
+//  ListingApplicationTests.swift
+//  ListingApplicationTests
+//
+//  Created by Admin on 21/2/25.
+//
+
+import Testing
+@testable import ListingApplication
+
+struct ListingApplicationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
